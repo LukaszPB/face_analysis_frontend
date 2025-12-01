37a2f3a49ccdec903001c2e0c2f508ad6e6f79a4
LukaszPB <lukasz.kiersnowski.110752@student.pb.edu.pl>
2025-12-01 21:32:50 +0100

    corrected image path

--- src/components/TipDescription.vue
+++ src/components/TipDescription.vue
@@ -37,7 +37,7 @@ export default {
   },
   methods: {
     getImagePath() {
-        return `public/images/${this.description}/${this.title}.png`;
+        return `/images/${this.description}/${this.title}.png`;
     }
   }
 }
